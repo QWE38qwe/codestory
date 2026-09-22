60ad553086adafdf64a40cacfc003339f54288a9
hou <544609031@qq.com>
2026-09-22 21:17:45 +0800

    feat: launch v0.3 workspace

--- src/main.jsx
+++ src/main.jsx
@@ -1,8 +1,9 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import "@xyflow/react/dist/style.css";
-import App from "./App";
+import App from "./AppV03";
 import "./styles.css";
+import "./v03.css";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
